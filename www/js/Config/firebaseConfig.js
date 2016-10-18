44979755005ecb17c5bcd1fdeb4e470f349ec44a
Alexander Murphy <amurphy345@gmail.com>
2016-10-18 17:04:55 -0600

    added config

---
+++ www/js/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+angular.module('proto.configs', [])
+
+.constant("CONFIG", {
+  "FIREBASE_API": 'AIzaSyBmG4Vt8nWeHprx7umVYGzHgwMeT5nYm5Q',
+  "FIREBASE_AUTH_DOMAIN": 'fir-portfolio.firebaseapp.com',
+  "FIREBASE_DB_URL": 'https://fir-portfolio.firebaseio.com',
+  "FIREBASE_STORAGE": 'fir-portfolio.appspot.com',
+  "MESSAGE_SENDER_ID": '280581318571'
+});
