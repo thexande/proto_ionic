622cf65ea0711e3ce5ff026ce9c5e38a07e0ecb8
Alexander Murphy <amurphy345@gmail.com>
2016-10-18 13:12:54 -0600

    facebook login with firebase progress

--- www/js/Controllers/loginController.js
+++ www/js/Controllers/loginController.js
@@ -104,10 +104,26 @@ angular.module('proto.loginController', [])
       $scope.modalRegister = modal;
     });
     $scope.openRegister = function() {
+
       $scope.modalRegister.show();
     };
     $scope.closeRegister = function() {
       $scope.modalRegister.hide();
+    };
+
+    // Sign up facebook modal
+    $ionicModal.fromTemplateUrl('templates/welcome/RegisterFB.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modalRegisterFB = modal;
+    });
+    $scope.openRegisterFB = function() {
+
+      $scope.modalRegisterFB.show();
+    };
+    $scope.closeRegisterFB = function() {
+      $scope.modalRegisterFB.hide();
     };
 
     // Perform the login action when the user submits the login form
@@ -116,8 +132,73 @@ angular.module('proto.loginController', [])
         template: '<ion-spinner></ion-spinner>'
       });
 
+      $scope.firebaseEmailLogin = function(userLogin) {
+         firebase.auth().signInWithEmailAndPassword(userLogin.username, userLogin.password).then(function() {
+          // Sign-In successful.
+          //console.log("Login successful");
 
-      console.log(userLogin);
+
+
+
+          var user = firebase.auth().currentUser;
+
+          var name, email, photoUrl, uid;
+
+          if (user.emailVerified) { //check for verification email confirmed by user from the inbox
+
+            $ionicLoading.hide();
+            $state.go('tab.home');
+            $scope.closeLogin();
+            $scope.closeRegister();
+
+            name = user.displayName;
+            email = user.email;
+            photoUrl = user.photoURL;
+            uid = user.uid;
+
+            //console.log(name + "<>" + email + "<>" +  photoUrl + "<>" +  uid);
+
+            localStorage.setItem("photo", photoUrl);
+
+          } else {
+
+            alert("Email not verified, please check your inbox or spam messages")
+            return false;
+
+          } // end check verification email
+
+
+        }, function(error) {
+          // An error happened.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorCode);
+          if (errorCode === 'auth/invalid-email') {
+            alert('Enter a valid email.');
+            return false;
+          } else if (errorCode === 'auth/wrong-password') {
+            alert('Incorrect password.');
+            return false;
+          } else if (errorCode === 'auth/argument-error') {
+            alert('Password must be string.');
+            return false;
+          } else if (errorCode === 'auth/user-not-found') {
+            alert('No such user found.');
+            return false;
+          } else if (errorCode === 'auth/too-many-requests') {
+            alert('Too many failed login attempts, please try after sometime.');
+            return false;
+          } else if (errorCode === 'auth/network-request-failed') {
+            alert('Request timed out, please try again.');
+            return false;
+          } else {
+            alert(errorMessage);
+            return false;
+          }
+        });
+      }
+
+
 
       if ($document[0].getElementById("user_name").value != "" && $document[0].getElementById("user_pass").value != "") {
 
@@ -267,15 +348,21 @@ angular.module('proto.loginController', [])
             getFacebookProfileInfo(success.authResponse)
               .then(function(profileInfo) {
                 // For the purpose of this example I will store user data on local storage
-                UserService.setUser({
+                $scope.facebookUser = {
                   authResponse: success.authResponse,
                   userID: profileInfo.id,
                   name: profileInfo.name,
                   email: profileInfo.email,
                   picture: "http://graph.facebook.com/" + success.authResponse.userID + "/picture?type=large"
-                });
+                }
 
-                $state.go('app.home');
+                UserService.setUser($scope.facebookUser);
+
+
+                // firebase login with facebook data
+                $scope.openRegisterFB()
+
+                //$state.go('app.home');
               }, function(fail) {
                 // Fail get profile info
                 console.log('profile info fail', fail);
